851b4a9785a64377590b669f87bfa114afd40924
nekithp7 <nekithp7@gmail.com>
2016-06-15 21:13:02 +0300

    Auditory and Board entities

---
+++ PresentationForYou.DAL/Entities/Auditory.cs
@@ -0,0 +1,11 @@
+﻿namespace PresentationForYou.DAL.Entities
+{
+    class Auditory
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public string Adress { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ PresentationForYou.DAL/Entities/Board.cs
@@ -0,0 +1,11 @@
+﻿namespace PresentationForYou.DAL.Entities
+{
+    class Board
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Height { get; set; }
+        public double Width { get; set; }
+    }
+}
